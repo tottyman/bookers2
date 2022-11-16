6c3eac9e93eb2ac9367a361c3f5e5f4c44b5c4b2
ToshiyukiIsshiki <114905631+tottyman@users.noreply.github.com>
2022-11-16 07:09:27 +0000

    [finish] bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(current_user)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  get "/home/about" => "homes#about" ,as: 'about'
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  
 end
